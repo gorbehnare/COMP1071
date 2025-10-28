7baa492bd3ef3f6ffd6656f66e55f783fa5ad0d3
gnp0mb5s@duck.com <13162106+gorbehnare@users.noreply.github.com>
2025-10-28 16:47:23 -0400

    Update server-check.sh

--- server-check.sh
+++ server-check.sh
@@ -332,10 +332,10 @@ if [ "$skipUpdate" = "no" ]; then
 	if [ "$?" != "0" -a -s /root/server-check-new.sh ]; then
 		mv /root/server-check-new.sh /root/server-check.sh
 		chmod +x /root/server-check.sh
-		echo -e "${YW}--> A different version of the script was downloaded.${CL}"
-		#echo -e "--> The server-check.sh updated! ${YW}Please re-run the script.${CL}"
-		### A.E. ### just ask user to re-run the script for now. This was done to prevent an edge-case issue
-		/root/server-check.sh -s "$@"
+		echo -e "${CY}-->${CL} A different version of the script was downloaded."
+		echo -e "${CY}-->${CL} The server-check.sh updated! ${YW}Please re-run the script.${CL}"
+		### A.E. ### just ask user to re-run the script for now. 
+		#/root/server-check.sh -s "$@"
 		rm $logfile # this logfile is pointless, discard it
 		exit
 	else
